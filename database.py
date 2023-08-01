265ed2338f8095911b8a7a093fd8d60ba2db9c8d
Var <106396959+VAREnyab@users.noreply.github.com>
2023-08-01 09:04:07 +0530

    Add an empty folder

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ import mysql.connector
 def database():
     # Connect to MySQL database
     db = mysql.connector.connect(
-        host='aws.connect.psdb.cloud',
-        user='2navrj03w1pymyqaxr3u',
-        password='pscale_pw_Y2Ibnd175OXmR9FHvc9SJUNHY8NMA7JDd7I3IFH0rC9',
-        database='resume_parser'
+        host='host_name',
+        user='user_name',
+        password='password',
+        database='db_name'
     )
     return db
